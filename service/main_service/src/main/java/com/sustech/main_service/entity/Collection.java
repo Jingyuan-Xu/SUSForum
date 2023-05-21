a1b74fb2bbca3d314f3b78f316e631b94cb74d4b
Lynchrocket <lynchrocket@gmail.com>
2023-05-21 22:12:55 +0800

    update

--- service/main_service/src/main/java/com/sustech/main_service/entity/Collection.java
+++ service/main_service/src/main/java/com/sustech/main_service/entity/Collection.java
@@ -3,14 +3,11 @@ package com.sustech.main_service.entity;
 import lombok.Data;
 
 @Data
-public class Comment {
+public class Collection {
     String id;
-    String content;
-    String topicId;
-    String path;
     String userId;
-    Integer likes;
-    Boolean isAnonymous;
+    String topicId;
+    String articleId;
     String gmtCreate;
     String gmtModified;
 }
