1dee7594dfd3cef479f882139c2ab779ff7db43f
Lynchrocket <lynchrocket@gmail.com>
2023-05-31 20:58:18 +0800

    Create OrderControllerTest.java

---
+++ service/main_service/src/test/java/com/sustech/main_service/controller/OrderControllerTest.java
@@ -0,0 +1,109 @@
+package com.sustech.main_service.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class OrderControllerTest {
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void addOrder() throws Exception {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.put("name", Collections.singletonList("for_test"));
+        params.put("picture", Collections.singletonList("123456"));
+        params.put("publisher", Collections.singletonList("for_test"));
+        params.put("is_buyer", Collections.singletonList("true"));
+        params.put("contact", Collections.singletonList("123456"));
+        params.put("price", Collections.singletonList("123456"));
+        mockMvc.perform(MockMvcRequestBuilders.get("/order/add")
+                        .params(params)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+
+    @Test
+    void getAllOrder() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/order/getAll")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void buyOrder() throws Exception {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.put("id", Collections.singletonList("1"));
+        params.put("user_id", Collections.singletonList("8348635108338113213"));
+        params.put("contact", Collections.singletonList("for_test"));
+        mockMvc.perform(MockMvcRequestBuilders.get("/order/buy")
+                        .params(params)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void sellOrder() throws Exception {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.put("id", Collections.singletonList("1"));
+        params.put("user_id", Collections.singletonList("8348635108338113213"));
+        params.put("contact", Collections.singletonList("for_test"));
+        mockMvc.perform(MockMvcRequestBuilders.get("/order/sell")
+                        .params(params)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void deleteById() throws Exception {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.put("id", Collections.singletonList("1"));
+        mockMvc.perform(MockMvcRequestBuilders.get("/order/delete")
+                        .params(params)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+}
